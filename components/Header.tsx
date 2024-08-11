06778366151eedc659e91624d1f57b329a191a73
oluwamuibiclear <igbokweudoka@gmail.com>
2024-08-11 14:17:44 -0500

    header component

---
+++ components/Header.tsx
@@ -0,0 +1,60 @@
+
+
+
+import { HiOutlineBell, HiOutlineSearch, HiOutlineSearchCircle } from "react-icons/hi";
+import { BellIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+
+    const handleScroll = () => {
+       
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  },[]);
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          alt=""
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="header__link">Home</li>
+          <li className="header__link">Tv Shows</li>
+          <li className="header__link">Movies</li>
+          <li className="header__link">New & Popular</li>
+          <li className="header__link">My List</li>
+          <li className="header__link">Browse by Language</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center  space-x-4 text-sm font-light">
+        <HiOutlineSearchCircle className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <HiOutlineBell className="h-6 w-6" />
+        <Link href="/account">
+          <img src="https://rb.gy/g1pwyx" alt="" />
+        </Link>
+      </div>
+    </header>
+  );
+}
