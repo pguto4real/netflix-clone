9bc0b86c20180d65780cfd797bf0e1fbb2519295
oluwamuibiclear <igbokweudoka@gmail.com>
2024-08-12 00:16:29 -0500

    ui component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from "tailwindcss";
 
-const config: Config = {
+
+
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      extend: {
+        backgroundImage: {
+          'gradient-to-b':
+            'linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 29%,rgba(20,20,20,.58) 44%,#141414 68%,#141414 100%);',
+        },
       },
-    },
   },
-  plugins: [require("@designbycode/tailwindcss-text-shadow"),],
+  plugins: [require("@designbycode/tailwindcss-text-shadow"),require('tailwind-scrollbar-hide'),
+    require('tailwind-scrollbar')
+  
+  ],
 };
-export default config;
